f0cfccddea5a9b5b195f6f07c860cdee40e6d42f
yota-yamazaki <yota.yamazaki@rightcode.co.jp>
2025-11-19 18:03:31 +0900

    add:各アプリに名前

--- mysite/head_office_app/apps.py
+++ mysite/head_office_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class HeadOfficeAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'head_office_app'
+    verbose_name = '店舗管理'
