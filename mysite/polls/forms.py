0fdf8cf53f9c217ffea7d4d9d76f252e40aa3b3c
yota-yamazaki <yota.yamazaki@rightcode.co.jp>
2025-11-20 09:56:38 +0900

    fix: choice全体でバリデーションがかかるように

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -5,10 +5,7 @@ from . import models
 class ChoiceForm(forms.ModelForm):
     class Meta:
         model = models.Choice
-        fields = ["choice_text", "question"]
-
-class ChoiceAdminForm(ChoiceForm):
-    pass
+        fields = ["choice_text"]
 
     def clean_choice_text(self):
         choice_text = self.cleaned_data['choice_text']
@@ -16,10 +13,25 @@ class ChoiceAdminForm(ChoiceForm):
             raise forms.ValidationError("テキストにはpiyoを入れないでください")
         return choice_text
 
+class ChoiceAdminForm(ChoiceForm):
+    pass
+
+    class Meta:
+        fields = ["choice_text", "question"]
+
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = models.Question
         fields = ["question_text",]
+
+class QuestionEditForm(QuestionForm):
+    pass
+
+    def clean_question_text(self):
+        question_text = self.cleaned_data['question_text']
+        if "piyo" in question_text:
+            raise forms.ValidationError("テキストにはpiyoを入れないでください")
+        return question_text
 
 class QuestionAdminForm(QuestionForm):
     pass
