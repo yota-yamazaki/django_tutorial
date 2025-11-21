21a63cab02c35a68f66e9e1f3055b098dba69743
yota-yamazaki <yota.yamazaki@rightcode.co.jp>
2025-11-21 19:22:27 +0900

    fix: parametrize

--- mysite/polls/tests/test_model.py
+++ mysite/polls/tests/test_model.py
@@ -8,9 +8,10 @@ from polls.models import Question
 @pytest.mark.parametrize(
     ["time", "is_recently"],
     [
+        pytest.param(1, True, id="1h old"),
+        pytest.param(24, True, id="24h old"),
+        pytest.param(25, False, id="25h old"),
         pytest.param(0, True, id="now"),
-        pytest.param(24, True, id="24h later"),
-        pytest.param(25, False, id="25h later"),
         pytest.param(-1, False, id="1h future"),
     ],
 )
